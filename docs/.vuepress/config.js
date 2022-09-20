af5f4dd4e1ab8430f153c2a6ff1d8eab1026763e
liugezhou <18231133236@163.com>
2022-09-20 14:26:49 +0800

    add baidu tongji

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,69 @@
 module.exports = {
-  title: '六个周',
+  title: '每日前端',
   description: '脚手架 浏览器渲染原理 Github说明书 Webpack 前端面试',
   base: '/frontend-docs/',
+  head: [
+    [
+      'script',
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?67c6532f7fb6859ef32c9cddf2b83c37";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      </script>
+      `,
+    ],
+  ],
   themeConfig: {
     logo: '/time.png',
     displayAllHeaders: false,
     searchMaxSuggestions: 10,
     lastUpdated: '上次更新',
     sidebar: {
-      '/web/':['before','00', '01', '02','03','04','05','06','14','15','16','17','28','29','30','31','32','33'],
-      '/webpack/':['before','vim'],
-      '/everyday/':['today','history']
+      '/web/': [
+        'before',
+        '00',
+        '01',
+        '02',
+        '03',
+        '04',
+        '05',
+        '06',
+        '14',
+        '15',
+        '16',
+        '17',
+        '28',
+        '29',
+        '30',
+        '31',
+        '32',
+        '33',
+      ],
+      '/webpack/': ['before', 'vim'],
+      '/everyday/': ['today', 'history'],
     },
     nav: [
       { text: '首页', link: '/' },
       { text: '今日前端', link: '/everyday/today' },
       { text: '前端脚手架', link: '/web/before' },
       { text: '前端笔记', link: '/webpack/before' },
-      { text: '浏览器渲染原理', link: 'https://liugezhou.gitbook.io/liugezhou/' },
-      { text: '我的博客', link: 'https://blog.liugezhou.online' },
+      {
+        text: '浏览器渲染原理',
+        link: 'https://liugezhou.gitbook.io/liugezhou/',
+      },
+      { text: '他的博客', link: 'https://blog.liugezhou.online' },
     ],
-    repo: 'liugezhou',
+    repo: 'liugezhou/frontend-docs',
     repoLabel: 'Github',
     docsBranch: 'main',
     editLinks: true,
-    docsDir: "/docs/",
+    docsDir: '/docs/',
     editLinkText: '错误反馈',
-    smoothScroll: true
-  }
+    smoothScroll: true,
+  },
 }
